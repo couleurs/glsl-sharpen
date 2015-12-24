3f8225e2b83ee9c7367b9a77cd8f419b306d0514
Michael Feldstein <msfeldstein@gmail.com>
2015-12-24 13:15:38 -0800

    Check differences in color not just averages of all colors

--- index.glsl
+++ index.glsl
@@ -5,32 +5,35 @@ float threshold(in float thr1, in float thr2 , in float val) {
   return val;
 }
 
-// averaged pixel intensity from 3 color channels
-float average(in vec4 pix) {
-  return (pix.r + pix.g + pix.b) / 3.0;
+// averaged pixel difference from 3 color channels
+float diff(in vec4 pix1, in vec4 pix2) {
+  return (
+    abs(pix1.r - pix2.r) +
+    abs(pix1.g - pix2.g) +
+    abs(pix1.b - pix2.b)
+  ) / 3.0;
 }
 
-// returns pixel color
 float edge(in sampler2D tex, in vec2 coords, in vec2 renderSize){
   float dx = 1.0 / renderSize.x;
   float dy = 1.0 / renderSize.y;
-  float pix[9];
+  vec4 pix[9];
   
-  pix[0] = average(texture2D(tex, coords + vec2( -1.0 * dx, -1.0 * dy)));
-  pix[1] = average(texture2D(tex, coords + vec2( -1.0 * dx , 0.0 * dy)));
-  pix[2] = average(texture2D(tex, coords + vec2( -1.0 * dx , 1.0 * dy)));
-  pix[3] = average(texture2D(tex, coords + vec2( 0.0 * dx , -1.0 * dy)));
-  pix[4] = average(texture2D(tex, coords + vec2( 0.0 * dx , 0.0 * dy)));
-  pix[5] = average(texture2D(tex, coords + vec2( 0.0 * dx , 1.0 * dy)));
-  pix[6] = average(texture2D(tex, coords + vec2( 1.0 * dx , -1.0 * dy)));
-  pix[7] = average(texture2D(tex, coords + vec2( 1.0 * dx , 0.0 * dy)));
-  pix[8] = average(texture2D(tex, coords + vec2( 1.0 * dx , 1.0 * dy)));
+  pix[0] = texture2D(tex, coords + vec2( -1.0 * dx, -1.0 * dy));
+  pix[1] = texture2D(tex, coords + vec2( -1.0 * dx , 0.0 * dy));
+  pix[2] = texture2D(tex, coords + vec2( -1.0 * dx , 1.0 * dy));
+  pix[3] = texture2D(tex, coords + vec2( 0.0 * dx , -1.0 * dy));
+  pix[4] = texture2D(tex, coords + vec2( 0.0 * dx , 0.0 * dy));
+  pix[5] = texture2D(tex, coords + vec2( 0.0 * dx , 1.0 * dy));
+  pix[6] = texture2D(tex, coords + vec2( 1.0 * dx , -1.0 * dy));
+  pix[7] = texture2D(tex, coords + vec2( 1.0 * dx , 0.0 * dy));
+  pix[8] = texture2D(tex, coords + vec2( 1.0 * dx , 1.0 * dy));
 
   // average color differences around neighboring pixels
-  float delta = (abs(pix[1]-pix[7])+
-          abs(pix[5]-pix[3]) +
-          abs(pix[0]-pix[8])+
-          abs(pix[2]-pix[6])
+  float delta = (diff(pix[1],pix[7])+
+          diff(pix[5],pix[3]) +
+          diff(pix[0],pix[8])+
+          diff(pix[2],pix[6])
            )/4.;
 
   return threshold(0.25,0.4,clamp(3.0*delta,0.0,1.0));
